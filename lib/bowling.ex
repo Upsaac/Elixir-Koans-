e3ef4fa2b6eae90543a7e924a4f82a0e54c6f400
Dylan Isaac Mancilla <isaac_mancilla@MacBook-Air-de-Dylan.local>
2026-02-20 21:55:26 -0600

    Cuarto commit Game with one 'strike'

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -1,5 +1,10 @@
 defmodule Bowling do
   def score([]), do: 0
+  def score([[f, s, t]]) when f===s and s===t, do: f + s + t
+  def score([[10,nil],[ff|ll]|rest]), do: 10 + twoturns([[ff|ll]|rest]) + score([[ff|ll]|rest])
   def score([[f,s|_l],[ff|ll]|rest]) when f + s == 10 , do: f + s + ff + score([[ff|ll]|rest])
   def score([[f,s|_l]|rest]), do: f + s + score(rest)
+
+  def twoturns([[10,nil],[ff|_ll]|_rest]), do: 10 + ff
+  def twoturns([[f,s]|_rest])when is_integer(f) and is_integer(s), do: f + s
 end
--- test/bowling_test.exs
+++ test/bowling_test.exs
@@ -16,4 +16,8 @@ defmodule BowlingTest do
     assert Bowling.score(game) == 16
   end
 
+  test "one strike" do
+    game = [[10,nil],[3,4]] ++ List.duplicate([0, 0], 7) ++ [[0, 0, nil]]
+    assert Bowling.score(game) == 24
+  end
 end
