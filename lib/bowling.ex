8e2a718e61afcf43d79228393323f382d698bb34
Dylan Isaac Mancilla <isaac_mancilla@MacBook-Air-de-Dylan.local>
2026-02-20 22:15:34 -0600

    Quinto commit Perfect game

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -1,10 +1,13 @@
 defmodule Bowling do
   def score([]), do: 0
-  def score([[f, s, t]]) when f===s and s===t, do: f + s + t
-  def score([[10,nil],[ff|ll]|rest]), do: 10 + twoturns([[ff|ll]|rest]) + score([[ff|ll]|rest])
-  def score([[f,s|_l],[ff|ll]|rest]) when f + s == 10 , do: f + s + ff + score([[ff|ll]|rest])
-  def score([[f,s|_l]|rest]), do: f + s + score(rest)
+  def score([[f, s, nil]]), do: f + s
+  def score([[f, s, t]]), do: f + s + t
 
-  def twoturns([[10,nil],[ff|_ll]|_rest]), do: 10 + ff
+  def score([[10,nil],[ff|ll]|rest]), do: 10 + twoturns([[ff|ll]|rest]) + score([[ff|ll]|rest]) #strike
+  def score([[f,s|_l],[ff|ll]|rest]) when f + s == 10 , do: 10 + ff + score([[ff|ll]|rest]) #spare
+  def score([[f,s|_l]|rest]), do: f + s + score(rest) #frame normal
+
+  def twoturns([[f, s, _t] | _rest]), do: f + s
+  def twoturns([[10, nil], [ff | _ll] | _rest]), do: 10 + ff
   def twoturns([[f,s]|_rest])when is_integer(f) and is_integer(s), do: f + s
 end
